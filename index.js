c94520d73a8a2fc28316fa8bb792eb5ac3b87021
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Code your solution in this file
 function findMatching(drivers, string){
-  return drivers.filter(function (name){ 
+  return drivers.filter(function (name){
     return name===string || name===string.toLowerCase();
   });
 }
-/*
-function fuzzyMatch(drivers, string){
 
+function fuzzyMatch(drivers, string){
+  let lengthOfName = string.length;
+  return drivers.filter(function(name){
+    return name.slice(0, lengthOfName)===string;
+  });
 }
-*/
+
 function matchName(drivers, string){
-  return drivers.filter(function(name){ 
+  return drivers.filter(function(name){
     return name===string;
   });
 }
