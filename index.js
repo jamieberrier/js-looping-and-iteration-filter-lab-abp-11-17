6ad28a243c7962c379e31d2666348c9fb31fb57f
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, string){
-  return drivers.filter(function (name){ return name===string || name===string.toLowerCase();});
+  return drivers.filter(function (name){ 
+    return name===string || name===string.toLowerCase();
+  });
 }
 /*
 function fuzzyMatch(drivers, string){
@@ -8,6 +10,7 @@ function fuzzyMatch(drivers, string){
 }
 */
 function matchName(drivers, string){
-  return drivers.filter(function(name){ return name===string;});
-  
+  return drivers.filter(function(name){ 
+    return name===string;
+  });
 }
