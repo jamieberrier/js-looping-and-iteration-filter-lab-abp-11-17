dc463b0299704372e58d9b72af0920349498fb17
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string){
-  return drivers.filter(function (name){ return name===string;});
+  return drivers.filter(function (name){ return name===string && string.toLowerCase();});
 }
 /*
 function fuzzyMatch(drivers, string){
